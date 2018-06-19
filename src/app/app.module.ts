cfb3c2e0d793fe23d0329a27076bc93e63518753
Jose Guillermo <jguillermo@outlook.com>
2018-06-19 01:32:12 -0500

    feat(template): carga inicial de la plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
+import {BreadcrumbsComponent} from './shared/breadcrumbs/breadcrumbs.component';
+import {APP_ROUTES} from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
